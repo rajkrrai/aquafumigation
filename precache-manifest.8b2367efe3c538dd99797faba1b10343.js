11df2a3334864bc01d3e49ed8ea6224c6363ceab
rajkrrai <64032301+rajkrrai@users.noreply.github.com>
2021-11-11 13:47:50 +0100

    Updates

---
+++ precache-manifest.8b2367efe3c538dd99797faba1b10343.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b419d1bf28a30e6eedd06a858633833",
+    "url": "/aquafumigation/index.html"
+  },
+  {
+    "revision": "172c6bd5d6cfc4e681d4",
+    "url": "/aquafumigation/static/css/main.56d66fdc.chunk.css"
+  },
+  {
+    "revision": "b44705b6ca0c577d57d2",
+    "url": "/aquafumigation/static/js/2.ffa1b988.chunk.js"
+  },
+  {
+    "revision": "0569f079e111a6d4f0bf8418a634cb07",
+    "url": "/aquafumigation/static/js/2.ffa1b988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "172c6bd5d6cfc4e681d4",
+    "url": "/aquafumigation/static/js/main.437bfb55.chunk.js"
+  },
+  {
+    "revision": "a73e78511a2f76924a7f",
+    "url": "/aquafumigation/static/js/runtime-main.9de141d4.js"
+  },
+  {
+    "revision": "ae948b46dd8497fbd1b9bd96c45ae86e",
+    "url": "/aquafumigation/static/media/corona4.ae948b46.jpg"
+  },
+  {
+    "revision": "455967efd6801e7614e1f0f8cd3eeb8b",
+    "url": "/aquafumigation/static/media/disinfectant1_n.455967ef.jpg"
+  },
+  {
+    "revision": "147ea50818c0d4a904c9299bf810c833",
+    "url": "/aquafumigation/static/media/gopalkrishna.147ea508.JPG"
+  },
+  {
+    "revision": "675f8e6a44db9c655f7a5a733a6456e4",
+    "url": "/aquafumigation/static/media/grainPesting.675f8e6a.JPG"
+  },
+  {
+    "revision": "d0d18f087e1cb09cf901f81aa01872bd",
+    "url": "/aquafumigation/static/media/logistics2_n.d0d18f08.jpg"
+  },
+  {
+    "revision": "f366d04936e117be6be2fe4f2a2eb511",
+    "url": "/aquafumigation/static/media/logoipsum-logo-4.f366d049.svg"
+  },
+  {
+    "revision": "145d2574d60c42beaf90b3c5de66b521",
+    "url": "/aquafumigation/static/media/sviswanathan.145d2574.JPG"
+  },
+  {
+    "revision": "5c8121ed592278f195a5f90b503628f8",
+    "url": "/aquafumigation/static/media/training.5c8121ed.JPG"
+  }
+]);
